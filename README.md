f109c46c63bd39db3c5bc2da3ac6f15be2c128fc
lxhyl <3529725395@qq.com>
2019-12-09 20:43:47 +0800

    add pro.js

---
+++ README.md
@@ -0,0 +1,25 @@
+
+**使用**   
+
+<<<<<<< HEAD
+下载   
+`git clone git@github.com:zpfnb/lxhyl-cli.git`
+
+安装   
+`npm install`   
+
+运行   
+`npm run serve`      
+=======
+下载    
+`git clone git@github.com:zpfnb/lxhyl-cli.git`   
+
+安装   
+`npm install`      
+
+运行   
+`npm run serve`   
+>>>>>>> 52e2bd1bde1a8330a4b7e90e796b00e1ced0b9ea
+
+打包   
+`npm run build`
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+//axios
+import axios from 'axios';
+Vue.prototype.$axios = axios;
+//允许跨域
+axios.defaults.withCredentials =true;
+
+//vue-router
+import VueRouter from 'vue-router';
+// import router from './router/router';
+Vue.use(VueRouter);
+
+//vuex
+// import store from './store/index'
+
+//element
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+new Vue({
+  // router,
+  // store,
+  render: h => h(App),
+}).$mount('#app')
